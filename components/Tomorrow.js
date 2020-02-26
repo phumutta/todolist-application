cc5ce1ffeddf07b83704aa3a7a3b9734343219e3
oat phattaraphon <phattaraphon.c@ku.th>
2020-02-27 04:44:07 +0700

    move code on here

--- components/Tomorrow.js
+++ components/Tomorrow.js
@@ -1,19 +1,140 @@
 import React, { Component } from 'react';
-import { ScrollView, StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, Image,} from "react-native";
+import { ScrollView, StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, Image,AsyncStorage} from "react-native";
 import Constants from "expo-constants";
 import { Container, Header, Title, Button, Icon, Content, InputGroup, Input } from 'native-base';
-// import ActionButton from 'react-native-action-button';
+import ActionButton from 'react-native-action-button';
+import Items2 from './Items2'
+import * as firebase from 'firebase';
+import '@firebase/firestore';
+import database from './Database3';
 
 
+export default class Tomorrow extends React.Component {
+
+  state = {
+    email: '',
+    message:'',
+    time:'',
+    Date:'',
+    Priority:''
+  }
+
+  onFocusFunction=async()=>{
+    this.setState({email:await AsyncStorage.getItem('@email')})
+    this.update()
+  }
+  // update (){
+  //   this.todo.update();
+
+  // };
+
+  componentDidMount(){
+
+    this.onFocusFunction();
+    let date = new Date().getDate(); //Current Date
+    var month = new Date().getMonth() + 1; //Current Month
+    var year = new Date().getFullYear(); //Current Year
+    var hours = new Date().getHours(); //Current Hours
+    var min = new Date().getMinutes(); //Current Minutes
+    var sec = new Date().getSeconds(); //Current Seconds
+    // this.setState({time:hours+":"+min+":"+sec})
+    // console.log(this.state.time)
+    this.setState({time:firebase.firestore.FieldValue.serverTimestamp()})
+    this.setState({Date:date + '/' + month + '/' + year})
+    
+  
+   
+ }
+  
+
+  onChangeText = message => this.setState({ message });
+
+  onPressAdd = async() => {
+    await this.addText();
+    this.setState({ text:null});
+
+    this.textInput.clear() 
+  };
+  addText=async()=>{
+    
+    Message={
+      message:this.state.message,
+      time: this.state.time,
+      Date:this.state.Date,
+      status:'1',
+      
+      id:''
+    }
+    console.log(this.state.email)
+   await  database.addMessageToday(this.state.email,Message,this.addMessageSuccess,this.addMessageFail)
+
+  }
+addMessageSuccess=async(id)=>{
+  
+  console.log("Successsssssssss");
+  await database.updateID(id,this.state.email,this.updateSuccess,this.updateFail)
+  this.update();
+  // await database.readMessage(this.state.email,this.state.Date,this.readMessageSuccess,this.readMessageFail)
+
+  
+}
+addMessageFail=async()=>{
+
+  console.log("addMessageFail");
+}
+async updateSuccess(){
+  
+  console.log("updateID");
+  await this.update();
+}
+updateFail(){
+  console.log("FailUpdate");
+}
+delete_Complete=async (id)=>{
+  await database.updateStatus(id,this.state.email,this.updateSuccess,this.updateFail)
+  // await database.deleteTask(this.state.email,id,this.deleteSuccess,this.deleteFail);
+  //this.onPressTrack();
+  await this.update();
+  
+  
+}
+deleteSuccess(){
+  
+  console.log("del success")
+}
+deleteFail(){
+  console.log("del fail")
+}
+    
+
+  readMessageSuccess=async(doc)=>{
+    console.log("message :" +doc.message);
+  
+  }
+  readMessageFail=async()=>{
+    console.log("read F");
+  }
+ async update(){
+    await this.todo.update();
+
+  };
 
 
-export default class Tomorrow extends Component {
+  
+
   onPressBack(){
     this.props.navigation.navigate('Main1')
  }
+ onPressEdit(){
+  this.props.navigation.navigate('Edit')
+ 
+
+}
+
     render() {
         return (
             <Container>
+              
             <Header >
               <View style = { styles.MainContainer1}>
                 <Button transparent onPress={()=>this.onPressBack()}>
@@ -21,33 +142,111 @@ export default class Tomorrow extends Component {
                 </Button>
               </View>
               <View style = { styles.MainContainer2 }>
+                {/* <Title>Today</Title> */}
                 <Title>Tomorrow</Title>
               </View>
               <View style={{flex: 1, alignItems: 'center',justifyContent: 'center', left: 15}}>
-                <Image style={{width: 20, height: 20}}
+                <Image style={{width: 20, height: 20}} 
                 source={{uri: 'https://sv1.picz.in.th/images/2020/01/22/RCoeNt.png' }}
               />
               </View>
             </Header>
 
-            <View style={{ flex:1,justifyContent:'center',marginTop:16,}}>
-              <View style={{flex: 1, flexDirection: 'row', alignSelf:'center', }}>
-               <View>
-                  <TextInput 
+            <View style={{ flex:1,flexDirection:'column',justifyContent:'center',marginTop:16, alignContent: 'center'}}>
+            <View style={{flex: 1, flexDirection: 'column', alignSelf:'center', marginTop:16,justifyContent:'center' , alignContent: 'center'}}>
+                <View style ={{flexDirection:'row'}}>
+                  <TextInput
+                  ref={input => { this.textInput = input }} 
                   style={styles.txtIn2}
                   placeholder="+ Add a task..."
                   onChangeText={this.onChangeText}
-                  /> 
+                  />
+                
+                  <TouchableOpacity
+                    style={styles.btn_register}
+                    onPress={this.onPressAdd}>
+                    <Text style={{fontSize:20, color:'#ffffff',textAlign:'center'}}>+</Text>
+                  </TouchableOpacity>
+                  
+                </View>
+                <View style={{flex:7,flexDirection:'column',justifyContent:'center' ,alignContent:'center',backgroundColor:'transparent',marginTop:"5%"}}>
+                  <ScrollView style={styles.listArea}>
+                        
+                        <Items2
+                            ref={todo => (this.todo = todo)}
+                            onPressTodo={this.delete_Complete}
+                            onPressTodo2={() => this.props.navigation.navigate('timer', { name: 'timer' })}
+                            onPressTodo3={() => this.props.navigation.navigate('Edit', { name: 'Edit' })}
+                              />
+                        
+                </ScrollView>
+                </View>
+            
+       
+              </View>
+             
+                   
+                  
+
+              {/* <View style={{marginLeft:"10%",alignItems:'flex-start',justifyContent:'flex-start',backgroundColor:"#666666"}}>
+                  <ScrollView style={styles.listArea}>
+                      
+                    <Items2
+                        ref={todo => (this.todo = todo)}
+                        onPressTodo={this.delete_Complete}
+                          />
+                    
+                   </ScrollView>
+                
+              </View> */}
+              {/* <View style={{flex: 1, flexDirection: 'row', alignSelf:'center', }}>
+                <View>
+                  <TextInput 
+                   ref={input => { this.textInput = input }} 
+                  style={styles.txtIn2}
+                  placeholder="+ Add a task..."
+                  onChangeText={this.onChangeText}
+                  />
+                </View>
+                <View>
+                  <TouchableOpacity
+                    style={styles.btn_register}
+                    onPress={this.onPressAdd}>
+                    <Text style={{fontSize:20, color:'#ffffff',textAlign:'center'}}>+</Text>
+                  </TouchableOpacity>
                 </View>
               </View>
+              
+              <View style={{flex: 7,flexDirection:'column', marginTop:16}}>
+                  <ScrollView style={styles.listArea}>
+                      
+                    <Items2
+                        ref={todo => (this.todo = todo)}
+                        onPressTodo={this.delete_Complete}
+                          />
+                    
+                   </ScrollView>
+                
+              </View> */}
 
-              <View style={{flex: 15,alignItems: 'center',justifyContent: 'center' }}>
+              {/* <View style={{flex: 15,alignItems: 'center',justifyContent: 'center' }}>
                 <Image style={{ width: 200, height: 152.77, }} source={{ uri: 'https://sv1.picz.in.th/images/2020/01/22/R2bmVk.png' }}/>
                 <Text style={{ color: '#666666' , marginTop:10, textAlign:'center' }}>You’re all done for today! #TodoblackZero{'\n'}Enjoy your night.</Text>
-              </View>
+              </View> */}
 
                {/* <ActionButton buttonColor="rgba(75,21,184,2)" position="center"></ActionButton> */}
 
+               {/* <ActionButton buttonColor="rgba(231,76,60,1)">
+              <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+                <Icon name="md-create" style={styles.actionButtonIcon} />
+              </ActionButton.Item>
+              <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
+                <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
+              </ActionButton.Item>
+              <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
+                <Icon name="md-done-all" style={styles.actionButtonIcon} />
+              </ActionButton.Item>
+            </ActionButton> */}
              </View>
 
             </Container>
@@ -82,6 +281,7 @@ const styles = StyleSheet.create({
   },
   listArea: {
     backgroundColor: "transparent",
+
     flex: 1,
     // paddingTop: 16
   },
@@ -142,7 +342,7 @@ const styles = StyleSheet.create({
 
     txtIn2: {
       alignItems: 'center',
-      width:350,
+      width:300,
       height:55,
       backgroundColor: 'transparent',
       padding: 16,
@@ -158,7 +358,7 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       width:50,
       height:50,
-      backgroundColor: '#000000',
+      backgroundColor: '#5B3E96',
       padding: 16,
       marginRight:16,
       borderRadius: 50,
@@ -170,7 +370,7 @@ const styles = StyleSheet.create({
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
-      right: 10  
+      right: 10 , 
     },
       
     MainContainer2:{
