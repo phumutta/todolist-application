d2a329804e4e88e50d8091aae285308ee9d79574
oat phattaraphon <phattaraphon.c@ku.th>
2020-03-13 08:27:09 +0700

    initial commit for group page (++)

--- components/Group.js
+++ components/Group.js
@@ -75,6 +75,9 @@ export default class Group extends Component {
   onPressBack(){
     this.props.navigation.navigate('Main1')
  }
+ onPressGroupDetail(){
+  this.props.navigation.navigate('GroupDetail')
+}
 
  onPressCreateNewGroup() {
   this.props.navigation.navigate('CreateNewGroup')
@@ -127,6 +130,12 @@ async update(){
                 </View>
               </TouchableOpacity>
 
+              <TouchableOpacity onPress={()=>this.onPressGroupDetail()}>
+                <View  style={{flex: 1, alignItems: 'center',justifyContent: 'center'}}>
+                  <Image style={{width: 20, height: 20}}source={{uri: 'https://sv1.picz.in.th/images/2020/01/26/RHjrif.png' }}/>
+                </View>
+              </TouchableOpacity>
+
             </Header>
 
 
@@ -167,36 +176,10 @@ async update(){
             </View>
           </TouchableOpacity>
           
-          </View>
+          </View> */}
           
 
-          <View style={{flex:0.125,flexDirection:'row',backgroundColor:'#ffffff', alignItems:'center', borderBottomColor: '#F6F6F6',borderBottomWidth: 1,marginTop:'2%'}} >
-            <Image style={{marginLeft:'10%', marginRight:'5%' ,width:30,height:30}} source={{uri:'https://sv1.picz.in.th/images/2020/01/26/RHl31W.png'}}/>
-                <View style={{flex:1, flexDirection: 'column'}} >
-                  <Text style={{fontSize:18,color:'#171D33',marginLeft:2,marginEnd:3,alignItems:'center',justifyContent:'center', }}>Dek 62</Text>
-                  <Text style={{fontSize:12,marginTop:3,color:'#C4C4C4'}}>มาเข้ากลุ่มเด็กแอด 62 กันเถอะ</Text>
-                </View>
-                <TouchableOpacity style={{marginRight:'8%'}}>
-                  <View  style={{flexDirection:'row-reverse',backgroundColor:'#ffffff',justifyContent: 'center',borderRadius:10, borderWidth:1,borderColor:'#F0ECFC', width:70, backgroundColor:'#F0ECFC'}}>
-                    <Text style={{fontSize:16,margin:'3%',color:'#6F41E9', justifyContent:'center'}}>Join</Text>
-                  </View>
-                </TouchableOpacity>
-            </View>
-
-          <View style={{flex:0.125,flexDirection:'row',backgroundColor:'#ffffff', alignItems:'center', borderBottomColor: '#F6F6F6',borderBottomWidth: 1,}} >
-            <Image style={{marginLeft:'10%', marginRight:'5%' ,width:30,height:30}} source={{uri:'https://sv1.picz.in.th/images/2020/01/26/RHl31W.png'}}/>
-                <View style={{flex:1, flexDirection: 'column'}} >
-                  <Text style={{fontSize:18,color:'#171D33',marginLeft:2,marginEnd:3,alignItems:'center',justifyContent:'center', }}>Dek 63</Text>
-                  <Text style={{fontSize:12,marginTop:3,color:'#C4C4C4'}}>มาเข้ากลุ่มเด็กแอด 63 กันเถอะ</Text>
-                </View>
-                <TouchableOpacity style={{marginRight:'8%'}}>
-                  <View  style={{flexDirection:'row-reverse',backgroundColor:'#ffffff',justifyContent: 'center',borderRadius:10, borderWidth:1,borderColor:'#F0ECFC', width:70, backgroundColor:'#F0ECFC'}}>
-                    <Text style={{fontSize:16,margin:'3%',color:'#6F41E9', justifyContent:'center'}}>Join</Text>
-                  </View>
-                </TouchableOpacity>
-            </View>
-
-          <View style={{flex:0.125,flexDirection:'row',backgroundColor:'#ffffff', alignItems:'center', borderBottomColor: '#F6F6F6',borderBottomWidth: 1,}} >
+          {/* <View style={{flex:0.125,flexDirection:'row',backgroundColor:'#ffffff', alignItems:'center', borderBottomColor: '#F6F6F6',borderBottomWidth: 1,}} >
             <Image style={{marginLeft:'10%', marginRight:'5%' ,width:30,height:30}} source={{uri:'https://sv1.picz.in.th/images/2020/01/26/RHl31W.png'}}/>
                 <View style={{flex:1, flexDirection: 'column'}} >
                   <Text style={{fontSize:18,color:'#171D33',marginLeft:2,marginEnd:3,alignItems:'center',justifyContent:'center', }}>Dek 64</Text>
@@ -245,23 +228,22 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: Constants.statusBarHeight
   },
-
   title: {
-      fontSize: 16,
-      color: '#FFFFFF',
+    fontSize: 16,
+    color: '#FFFFFF',
   },
   container_text: {
-      flex: 1,
-      flexDirection: 'column',
-      borderRadius:10,
-      paddingLeft:5,
-      justifyContent: 'center',
-      backgroundColor:'#1F9BF1',
+    flex: 1,
+    flexDirection: 'column',
+    borderRadius:10,
+    paddingLeft:5,
+    justifyContent: 'center',
+    backgroundColor:'#1F9BF1',
   },
   description: {
-      fontSize: 11,
-      fontStyle: 'italic',
-      color: '#FFFFFF',
+    fontSize: 11,
+    fontStyle: 'italic',
+    color: '#FFFFFF',
   },
   photo2: {
     height: 30,
@@ -271,33 +253,29 @@ const styles = StyleSheet.create({
     marginTop:5,
     borderWidth:1,
     borderRadius: 1,
-
-  alignItems: 'center',
-  justifyContent: 'center'
-
-}, photo3: {
-  height: 25,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }, 
+  photo3: {
+    height: 25,
     width: 25,
     marginRight:5,
     marginLeft:5,
     marginTop:5,
-  alignItems: 'center',
-  justifyContent: 'center'
-
-
-},
-  photo: {
-      height: 45,
-      width: 45,
-      marginRight:5,
-      marginLeft:5,
-      marginTop:5,
-      borderWidth:10,
-      borderRadius: 50,
-      flex:1,
     alignItems: 'center',
     justifyContent: 'center'
-
+  },
+  photo: {
+    height: 45,
+    width: 45,
+    marginRight:5,
+    marginLeft:5,
+    marginTop:5,
+    borderWidth:10,
+    borderRadius: 50,
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   Text:{
     color:"#000000",
@@ -307,8 +285,8 @@ const styles = StyleSheet.create({
     marginEnd : 2,
     alignItems: 'center',
     justifyContent: 'center'
-
-  },Text2:{
+  },
+  Text2:{
     color:"#5B3E96",
     fontSize: 17,
     marginTop:5,
@@ -329,15 +307,12 @@ const styles = StyleSheet.create({
   },
   photo4: {
     height: 18,
-      width: 18,
-      marginRight:20,
-      flexDirection: 'row-reverse',
-      alignItems:'center'
-
-  
-  
+    width: 18,
+    marginRight:20,
+    flexDirection: 'row-reverse',
+    alignItems:'center'
   },
 
 
-
+  
 });
