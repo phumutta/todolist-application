e058bec2f919eb34125d9b9831a129cf6baeacdf
oat phattaraphon <phattaraphon.c@ku.th>
2020-03-26 02:57:04 +0700

    adding marginBottom for Scrolling on GroupDetail Page

--- components/Items_GroupNew.js
+++ components/Items_GroupNew.js
@@ -134,7 +134,7 @@ export default class Items_GroupNew extends React.Component {
     return (
 
 
-      <View style={{flex:1, alignItems:'center', flexDirection:'column', justifyContent:'center', backgroundColor:"transparent", marginBottom:28}}>
+      <View style={{flex:1, alignItems:'center', flexDirection:'column', justifyContent:'center', backgroundColor:"transparent", marginBottom:33}}>
 
         {items.map(({ date, id, message, time,user ,uri,Des}) => (
 
