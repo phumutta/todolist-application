78574a62542064e4b44d6070d4e5f76c3cfd8a13
Phumutta <Phumutta@gmail.com>
2020-03-27 10:47:54 +0700

    update placeholder

--- components/Edit_Tomorrow.js
+++ components/Edit_Tomorrow.js
@@ -325,7 +325,7 @@ getRepeat = () =>{
                     <SafeAreaView forceInset={{top:'always',horizontal:'never'}} >
                         <View style={{flex:1,marginTop:'8%',height:700,alignItems:'center',zIndex:1, paddingBottom:15}}>
                           {/* <Text style={{width:'80%', fontSize:18, marginTop:'3%', color:'#696969'}}>{moment(now).format('MMMM Do YYYY, h:mm a')}</Text> */}
-      <TextInput underlineColorAndroid='#4CAF50' style={{width:'80%', fontSize:18,color:'#696969'}} multiline={true} numberOfLines={10} placeholder='Description... ' onChangeText={this.onChangeDes}>{this.state.des}</TextInput>
+      <TextInput underlineColorAndroid='#4CAF50' style={{width:'80%', fontSize:18,color:'#696969'}} multiline={true} numberOfLines={10}  onChangeText={this.onChangeDes}>{this.state.des}</TextInput>
                         </View>
                     </SafeAreaView>
                 </ScrollView>
